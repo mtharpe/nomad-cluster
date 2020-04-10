97624609b141ec1c87c4523e1fb7237580174761
pgryzan <patrick.gryzan@gmail.com>
2020-04-10 14:47:08 -0400

    removed test

--- outputs.tf
+++ outputs.tf
@@ -14,7 +14,5 @@ output "outputs" {
         consul                      = "http://${google_compute_instance.hashistack_server[0].network_interface.0.access_config.0.nat_ip}:8500"
         nomad                       = "http://${google_compute_instance.hashistack_server[0].network_interface.0.access_config.0.nat_ip}:4646"
         ssh_hashistack_server       = "ssh -o stricthostkeychecking=no -i ${ var.ssh.private_key } ${ var.ssh.username }@${ google_compute_instance.hashistack_server[0].network_interface.0.access_config.0.nat_ip } -y"
-        servers                     = local.servers
-        command                     = "sudo /tmp/hashistack-init.sh -d '${local.data_center}' -c '${local.consul_version}' -n '${local.nomad_version}' -a 'server' -r '${local.servers}' -s ${local.server_count}"
     }
 }
